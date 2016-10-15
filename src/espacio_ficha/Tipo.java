8bd7b38397e22d4562bb9d54bd859550f6e0a78b
Juan Felipe Fontecha V. <>
2016-10-15 13:25:14 -0500

    Tablero, fichas y casillas.

---
+++ src/espacio_ficha/Tipo.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package espacio_ficha;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Tipo {
+    private int id;
+    private int estado;
+    private int numero;
+
+    public Tipo(int id, int estado, int numero) {
+        this.id = id;
+        this.estado = estado;
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+    
+}
