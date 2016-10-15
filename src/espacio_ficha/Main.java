e0b857eb266128e96fc4f58e58d45ad719a1b34b
Juan Felipe Fontecha V. <USUARIO@USUARIO-PC>
2016-10-15 13:28:10 -0500

    Tablero, fichas y casillas.

---
+++ src/espacio_ficha/Main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package espacio_ficha;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Dado d1 = new Dado(1);
+        Dado d2 = new Dado(2);
+        
+/*
+        d2.lanzarDado();
+
+        System.out.println(d1.lanzarDado() + "\t" + d2.lanzarDado());
+        */
+        
+    }
+    
+}
